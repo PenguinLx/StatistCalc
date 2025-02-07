dfbe27f09a7cb4b8091c2dcbff4bbbccc047a13a
PenguinLx <julianol.schaeffer@gmail.com>
2025-02-07 16:03:51 -0300

    first commit of the 'StatistCalc' project(just starting the crud and making some tests)

---
+++ src/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CalculadoraEstatistica
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //SÓ TESTES POR ENQUANTO
+              //SIMPLES//
+      Simples simples = new Simples(19, 20);
+      Console.WriteLine("Hello");
+      double x = simples.X = 91;
+      int f = simples.FreqAbs = 92;
+      double x2 = simples.X = 56;
+      int f2 = simples.FreqAbs = 10;
+    //Console.WriteLine("new x: " + simples.X + "\nnew FreqAbs: " + simples.FreqAbs);
+    Tabela tabela= new Tabela();
+    tabela.AddTabela(x,f);
+    tabela.AddTabela(x2,f2);
+    tabela.ExibirTabela();
+
+              //INTERVALO//
+    // IntervaloClasse intervaloClasse= new IntervaloClasse();
+    // intervaloClasse.Linf = 60;
+    // intervaloClasse.Lsup = 70;
+    // Console.WriteLine("Linf: " + intervaloClasse.XInterv[0] + "\nLsup: " + intervaloClasse.XInterv[1]);
+
+    }
+  }
+}
